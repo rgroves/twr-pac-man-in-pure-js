0f972fd404ff92d210e6a320f658f6b1b10b5c36
Robert Groves <rwgdev@gmail.com>
2020-04-16 15:25:40 -0500

    Add ghosts to the game board.

--- app.js
+++ app.js
@@ -145,4 +145,28 @@ document.addEventListener("DOMContentLoaded", () => {
       squares[pacmanCurrentIndex].classList.remove("pac-dot");
     }
   }
+
+  // create our Ghost template
+  class Ghost {
+    constructor(className, startIndex, speed) {
+      this.className = className;
+      this.startIndex = startIndex;
+      this.speed = speed;
+      this.currentIndex = startIndex;
+      this.timerId = NaN;
+    }
+  }
+
+  let ghosts = [
+    new Ghost("blinky", 348, 250),
+    new Ghost("pinky", 376, 400),
+    new Ghost("inky", 351, 300),
+    new Ghost("clyde", 379, 500),
+  ];
+
+  // draw our ghosts onto the grid
+  ghosts.forEach((ghost) => {
+    squares[ghost.currentIndex].classList.add(ghost.className);
+    squares[ghost.currentIndex].classList.add("ghost");
+  });
 });
