b50be851cf70752b02eacc405d8ec91735b6d252
Robert Groves <rwgdev@gmail.com>
2020-04-16 12:57:06 -0500

    Add Pac-Man to game board.

--- app.js
+++ app.js
@@ -66,4 +66,9 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   createBoard();
+
+  // starting position of pac-man
+  let pacmanCurrentIndex = 490;
+
+  squares[pacmanCurrentIndex].classList.add("pac-man");
 });
