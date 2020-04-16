f19e6ed66b22e328e1f9cdbe96fb56096a6e39b2
Robert Groves <rwgdev@gmail.com>
2020-04-16 15:17:52 -0500

    Allow Pac-Man to eat pac-dots and update score.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   const scoreDisplay = document.getElementById("score");
   const width = 28; // 28 x 28 = 784 squares
+  let score = 0;
 
   // layout of grid and what is in the squares
   // prettier-ignore
@@ -130,7 +131,18 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     squares[pacmanCurrentIndex].classList.add("pac-man");
+
+    pacDotEaten();
   }
 
   document.addEventListener("keyup", movePacman);
+
+  // what happens when Pac-Man eats a pac-dot.
+  function pacDotEaten() {
+    if (squares[pacmanCurrentIndex].classList.contains("pac-dot")) {
+      score++;
+      scoreDisplay.innerHTML = score;
+      squares[pacmanCurrentIndex].classList.remove("pac-dot");
+    }
+  }
 });
